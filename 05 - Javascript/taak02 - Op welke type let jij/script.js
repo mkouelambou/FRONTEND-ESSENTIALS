4b08b2e1b5ab8431b6a8c163652f07ec72212388
Mike <mike.kouelambou@student.rocva.nl>
2020-10-28 16:22:34 +0100

    save

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,12 @@
+letmijnVoornaam = "Mike"
+letmijnAchternaam ="Koue-Lambou"
+let beroep = "Software Delevoper";
+let school = 'Roc van Amstelland';
+
+let naamStudent ="Mike"
+let leeftijdStudent = 17;
+let ikBenStemGrechtig = "null";
+let ikMagLegaalAlcoholKopen ="null";
+let ikMagBrommerRijden = "null";
+let SoftwareOntwikkeling = "Ik vind het een leuke opleiding";
+
---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,4 @@
+let mijnVoornaam ="Mike"
+let mijnAchternaam ="Koue-Lambou"
+let volledigeNaam = mijnVoornaam + mijnAchternaam;
+console.log(volledigeNaam);
