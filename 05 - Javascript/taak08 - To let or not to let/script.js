aaea6eea82c5bdc874d2067cdbe1e361d86865ff
Mike <mike.kouelambou@student.rocva.nl>
2020-10-29 11:44:53 +0100

    save changes

---
+++ 05 - Javascript/taak08 - To let or not to let/script.js
@@ -0,0 +1,11 @@
+let codingIsCool ="true";
+let codingIsCool = "false";
+console.log
+Boolean(true > false)  // returns true
+Boolean(false < true)  // returns false
+
+let mijnLeeftijd = "17";
+let jaarGetal = 17;
+let geboorteJaar = 2003;
+let geboorteJaar = geboorteJaar + jaarGetal;
+let geboorteJaar = jaarGetal + 2003;
