c903f91048666413c64647f4b8862b1431edf951
Mike <mike.kouelambou@student.rocva.nl>
2020-11-02 12:42:43 +0100

    commit

---
+++ opdracht/eindopdracht/script.js
@@ -0,0 +1,7 @@
+let persoon = {
+    voornaam : prompt("Wat is je naam?"),
+    achternaam : prompt("Wat is je achternaam?"),
+    leeftijd : prompt("Wat is je leeftijd?"),
+    email : prompt("Wat is je email?"),
+    woonplaats : prompt("Wat is je woonplaats?")
+};
