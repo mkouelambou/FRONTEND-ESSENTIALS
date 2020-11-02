2afe0b08ea906952d0c22890ad72f09e7f0fe7dd
Mike <mike.kouelambou@student.rocva.nl>
2020-11-02 20:31:10 +0100

    save changes

--- opdracht/eindopdracht/script.js
+++ opdracht/eindopdracht/script.js
@@ -1,7 +1,23 @@
-let bezoeker = {
+let bezoeker ={
     voornaam : prompt("Wat is je naam?"),
     achternaam : prompt("Wat is je achternaam?"),
     leeftijd : prompt("Wat is je leeftijd?"),
     email : prompt("Wat is je email?"),
-    woonplaats : prompt("Wat is je woonplaats?")
+    woonplaats : prompt("Wat woon je?"),
 };
+console.log(bezoeker);
+if (bezoeker.leeftijd ==16) {
+    document.body.style.backgroundColor = 'orange';
+    }
+    if (bezoeker.leeftijd ==17) {
+        document.body.style.backgroundColor = 'blue';
+        }
+        if (bezoeker.leeftijd ==18) {
+            document.body.style.backgroundColor = 'gold';
+            }
+            if (bezoeker.leeftijd ==19) {
+                document.body.style.backgroundColor = 'green';
+                }
+                if (bezoeker.leeftijd ==0) {
+                    document.body.style.backgroundColor = 'yellow';
+                    }
